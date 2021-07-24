26f1f968e9b3b40e17a4554ea39675e60ffd3c75
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-07-24 08:09:12 +0100

    switched fro rest api to graphql

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 import SiteHeader from './components/SiteHeader'
 import Category from './pages/Category';
 import Homepage from './pages/Homepage'
 import ReviewDetails from './pages/ReviewDetails';
 
+//apolloclient
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
 
 function App() {
   return (
     <Router>
+      <ApolloProvider client={client}>
     <div className="App">
      <SiteHeader />
      <Switch>
@@ -23,6 +32,7 @@ function App() {
      
      </Switch>
     </div>
+    </ApolloProvider>
     </Router>
   );
 }
